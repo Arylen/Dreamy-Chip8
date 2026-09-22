3f7a0f4a6b57bf8a365eea00c2675a9848fc71ca
Arylen <177881981+Arylen@users.noreply.github.com>
2026-09-21 20:16:11 -0500

    Add breaks on cases

--- src/core/emulation/Chip8Dasm.h
+++ src/core/emulation/Chip8Dasm.h
@@ -29,8 +29,10 @@ namespace dc8::core::emulation {
                 if (op.getN() == 0x6) return std::format("SHR  V{:X}, V{:X}", op.getX(), op.getY());
                 if (op.getN() == 0x7) return std::format("SUBN V{:X}, V{:X}", op.getX(), op.getY());
                 if (op.getN() == 0xE) return std::format("SHL  V{:X}, V{:X}", op.getX(), op.getY());
+                break;
             case 0x9:
                 if (op.getN() == 0x0) return std::format("SNE  V{:X}, V{:X}", op.getX(), op.getY());
+                break;
             case 0xA: return std::format("LD   I,  {:03X}", op.getNNN());
             case 0xB: return std::format("JP   V0, {:03X}", op.getNNN());
             case 0xC: return std::format("RND  V{:X}, {:02X}", op.getX(), op.getNN());
@@ -38,6 +40,7 @@ namespace dc8::core::emulation {
             case 0xE:
                 if (op.getNN() == 0x9E) return std::format("SKP  V{:X}", op.getX());
                 if (op.getNN() == 0xA1) return std::format("SKNP V{:X}", op.getX());
+                break;
             case 0xF:
                 if (op.getNN() == 0x07) return std::format("LD   V{:X}, DT", op.getX());
                 if (op.getNN() == 0x0A) return std::format("LD   V{:X}, K",  op.getX());
@@ -48,6 +51,7 @@ namespace dc8::core::emulation {
                 if (op.getNN() == 0x33) return std::format("LD   B,  V{:X}", op.getX());
                 if (op.getNN() == 0x55) return std::format("LD   @I, V{:X}", op.getX());
                 if (op.getNN() == 0x55) return std::format("LD   V{:X}, @I", op.getX());
+                break;
         }
 
         return std::format("UNK  {:02X} {:02X}", op.getHi(), op.getLo());
