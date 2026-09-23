bc7a230093bd7e0bb2d044936e85d2916be7ed7e
Arylen <177881981+Arylen@users.noreply.github.com>
2026-09-22 21:34:48 -0500

    Add basic tests

--- tests/Chip8OpTests.cpp
+++ tests/Chip8OpTests.cpp
@@ -24,3 +24,43 @@ TEST_CASE("Chip8Op Decodes N", "[Chip8Op]") {
 
     REQUIRE(op.getN() == 0x4);
 }
+
+TEST_CASE("Chip8Op Decodes NN", "[Chip8Op]") {
+    const dc8::core::emulation::Chip8Op op {
+        .raw = 0x1234,
+    };
+
+    REQUIRE(op.getNN() == 0x34);
+}
+
+TEST_CASE("Chip8Op Decodes NNN", "[Chip8Op]") {
+    const dc8::core::emulation::Chip8Op op {
+        .raw = 0x1234,
+    };
+
+    REQUIRE(op.getNNN() == 0x234);
+}
+
+TEST_CASE("Chip8Op Decodes Hi Byte", "[Chip8Op]") {
+    const dc8::core::emulation::Chip8Op op {
+        .raw = 0x1234,
+    };
+
+    REQUIRE(op.getHi() == 0x12);
+}
+
+TEST_CASE("Chip8Op Decodes Lo Byte", "[Chip8Op]") {
+    const dc8::core::emulation::Chip8Op op {
+        .raw = 0x1234,
+    };
+
+    REQUIRE(op.getLo() == 0x34);
+}
+
+TEST_CASE("Chip8Op Decodes Family", "[Chip8Op]") {
+    const dc8::core::emulation::Chip8Op op {
+        .raw = 0x1234,
+    };
+
+    REQUIRE(op.getFamily() == 0x1);
+}
