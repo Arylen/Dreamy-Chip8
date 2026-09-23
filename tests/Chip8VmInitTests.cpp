8b640b230f5176693126f6f981b42e855993d8a9
Arylen <177881981+Arylen@users.noreply.github.com>
2026-09-22 21:51:33 -0500

    PC actually starts at 0x200, silly

--- tests/Chip8VmInitTests.cpp
+++ tests/Chip8VmInitTests.cpp
@@ -1,11 +1,12 @@
 #include <format>
 #include <catch2/catch_test_macros.hpp>
 
+#include "core/emulation/Chip8Consts.h"
 #include "core/emulation/Chip8VM.h"
 
 TEST_CASE("PC inits to 0", "[Chip8VM]") {
     dc8::core::emulation::Chip8VM vm;
-    REQUIRE(vm.getPC() == 0);
+    REQUIRE(vm.getPC() == dc8::core::emulation::RomStartAddress);
 }
 
 TEST_CASE("I inits to 0", "[Chip8VM]") {
@@ -29,11 +30,18 @@ TEST_CASE("SoundTimer inits to 0", "[Chip8VM]") {
 }
 
 TEST_CASE("Memory inits to all 0", "[Chip8VM]") {
+    constexpr uint8_t EXPECTED = 0;
+
+    constexpr uint16_t FONT_START = dc8::core::emulation::FontStartAddress;
+    constexpr uint16_t FONT_END = dc8::core::emulation::FontEndAddress;
+
     dc8::core::emulation::Chip8VM vm;
-    uint8_t expected = 0;
     for (size_t address = 0; address < vm.getMem().size(); address++) {
+        if (address >= FONT_START && address <= FONT_END) {
+            continue;
+        }
         INFO(std::format("Memory Address: 0x{:04X}", address));
-        CAPTURE(address, expected);
-        REQUIRE(vm.getMem()[address] == expected);
+        CAPTURE(address, EXPECTED);
+        REQUIRE(vm.getMem()[address] == EXPECTED);
     }
 }
