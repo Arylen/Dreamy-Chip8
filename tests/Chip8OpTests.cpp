0621a74fb6af28a1942fb8fd469a273f22b959b1
Arylen <177881981+Arylen@users.noreply.github.com>
2026-09-22 21:24:10 -0500

    some test improvements

--- tests/Chip8OpTests.cpp
+++ tests/Chip8OpTests.cpp
@@ -1,10 +1,26 @@
 #include <catch2/catch_test_macros.hpp>
 #include "core/emulation/Chip8Op.h"
 
-TEST_CASE("Opcode decodes X", "[opcode]") {
+TEST_CASE("Chip8Op Decodes X", "[Chip8Op]") {
     const dc8::core::emulation::Chip8Op op {
         .raw = 0x1234,
     };
 
     REQUIRE(op.getX() == 0x2);
+}
+
+TEST_CASE("Chip8Op Decodes Y", "[Chip8Op]") {
+    const dc8::core::emulation::Chip8Op op {
+        .raw = 0x1234,
+    };
+
+    REQUIRE(op.getY() == 0x3);
+}
+
+TEST_CASE("Chip8Op Decodes N", "[Chip8Op]") {
+    const dc8::core::emulation::Chip8Op op {
+        .raw = 0x1234,
+    };
+
+    REQUIRE(op.getN() == 0x4);
 }
